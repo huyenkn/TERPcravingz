524a9cb63cdd55e74560cb7c125398ece729bbd5
Huyen Nguyen <53942631+huyenkn@users.noreply.github.com>
2019-12-02 13:16:46 -0500

    Update 0504_06_Query1.sql

--- Submitted Suite Documents/DML/0504_06_Query1.sql
+++ Submitted Suite Documents/DML/0504_06_Query1.sql
@@ -2,7 +2,7 @@
 --1. What is the average price of all restaurants in each city, in the order of descending average price, and then alphabetical order of city?
 
 
-SELECT r.resState as 'State' , r.resCity AS 'City', ROUND(AVG(CAST(r.resPrice AS FLOAT)), 2)
+SELECT r.resState as 'State' , r.resCity AS 'City', ROUND(AVG(CAST(r.resPrice AS FLOAT)), 2) AS 'average price'
 FROM Restaurant r
 GROUP BY r.resState, r.resCity
 HAVING AVG(r.resPrice) > 0
