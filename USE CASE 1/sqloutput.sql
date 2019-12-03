c7ee4bf4f0fb684beb720f60c23c29873aef0754
Aishwarya Bhangale <aishwaryabhangale1997@gmail.com>
2019-12-03 16:25:52 -0500

    Tableau and SQL use case updated

---
+++ USE CASE 1/sqloutput.sql
@@ -0,0 +1,7 @@
+SELECT r.stopId AS 'Stop ID' , s.stopName AS 'Stop Name', COUNT(r.resId) AS 'Number of restaurants'
+FROM Restaurant r, Stop s
+WHERE r.stopId = s.stopId
+AND r.walktimeToRes <= 15
+GROUP BY r.stopId, s.stopName
+HAVING COUNT(r.resId) >= 10 AND COUNT(r.resId) <= 100
+ORDER BY COUNT(r.resId) DESC
